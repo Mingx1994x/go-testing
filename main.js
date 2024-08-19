43fdf5a99bd1e54a0b671afe4898ee92bfa42f6f
Mingx1994x <mggxmingx@gmail.com>
2024-08-19 17:46:03 +0800

    test area 上線啦

---
+++ main.js
@@ -0,0 +1,26 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// import Swiper JS
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+
+// import AOS JS
+// import AOS from 'aos';
+
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import AOS styles 
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+// custom Swiper
+import "./src/js/custom-swiper.js";
+
+// custom AOS
+import "./src/js/custom-aos.js";
+
+console.log("Hello world!");
